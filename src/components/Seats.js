c01ab6cc1ea58325ddb6fc64195102bf9b2611af
Sylrel <abl00021@hotmail.com>
2023-04-11 17:57:35 +0200

    Actualizando 11 abril

--- src/components/Seats.js
+++ src/components/Seats.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Button, Container, Row, Col } from 'react-bootstrap';
 import { MdOutlineEventSeat } from 'react-icons/md';
 import BackButton from './BackButton';
 import firebaseApp from '../firebase-config';
-//import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
-import { onSnapshot, collection, addDoc, doc, updateDoc, deleteField, getFirestore } from 'firebase/firestore';
+import { onSnapshot, collection, addDoc, doc, getFirestore } from 'firebase/firestore';
 import { useParams } from 'react-router-dom';
 
 export default function CinemaRoom() {
@@ -13,6 +12,7 @@ export default function CinemaRoom() {
     const [selectedSeats, setSelectedSeats] = useState(0);
     const { title } = useParams();
     const [selectedPrice, setSelectedPrice] = useState(0);
+    const selectedRoom = useParams();  //Accedo a la url que esta en MovieRooms.js y luego esta variable la paso a la constante salaReserva y la leo en el document.write salaReserva.selectedRoom
 
     const db = getFirestore(firebaseApp);
 
@@ -23,19 +23,20 @@ export default function CinemaRoom() {
 
         setSelectedSeats(selectedSeats + (newSeats[index] ? 1 : -1));
         setSelectedPrice(selectedPrice + (newSeats[index] ? 5 : -5));
-        
-
     };
 
-   
 
     //Guardar los recibos en la cloud firestore lo puede configurar gracias a este video
     // https://www.youtube.com/watch?v=8idyed4aJEA&t=58s
 
     const saveReceipt = () => {
         const auth = getAuth(firebaseApp);
-        const butacasReservadas = "";
-        const salaReserva = "";                           //Tengo que arreglar como se coloca la sala elegida por el cliente al recibir el recibo en la base de datos
+        const butacasReservadas = seats                                         //Despues de arreglar la linea 16 por fin he podido filtrar las butacas seleccionadas y mostrarlas en el recibo
+            .map((seat, index) => (seat ? index + 1 : null))
+            .filter((seat) => seat !== null)
+            .join(", ");
+
+        const salaReserva = selectedRoom;                           //Arreglado linea 16
         const userEmail = auth.currentUser.email;
         const reciboRef = collection(db, "recibos");
         const ultimoReciboRef = doc(reciboRef, "ultimo");
@@ -46,6 +47,7 @@ export default function CinemaRoom() {
 
             const nuevoNumeroRecibo = ultimoNumeroRecibo + 1;
             const fechaReserva = new Date();
+
             // Guardar el recibo en la base de datos de Firebase
             addDoc(collection(db, "recibos"), {
                 numeroRecibo: nuevoNumeroRecibo,
@@ -59,66 +61,56 @@ export default function CinemaRoom() {
 
             alert("Recibo enviado a la bbdd");
 
-                /*Construir la URL con los datos del cliente y la información de la reserva
-                const baseUrl = window.location.origin;
-                const queryParams = `?email=${userEmail}&movie=${"movieName"}&seats=${selectedSeats}&sala=${salaReserva}&fecha=${fechaReserva}`;
-                const url = `${baseUrl}/receipt${queryParams}`;
-              
-                // Abrir la nueva ventana con la URL construida
-                window.open(url, '_blank');*/
+            //Intentar exportarlo a una funcion para aplicar estilos
+            //Hacer tambien que cuando envie el recibo deshabilite las butacas seleccionadas
 
-                const reciboContent = `
+           const reciboContent = `
                 <html>
                   <head>
                     <title>Recibo ${nuevoNumeroRecibo}</title>
                   </head>
                   <body>
                     <h1>Recibo ${nuevoNumeroRecibo}</h1>
-                    <p>Fecha de reserva: ${fechaReserva.toLocaleDateString()} a las ${fechaReserva.toLocaleTimeString()}</p>
-
-                    <p>Nombre de la pelicula: ${title}</p>
-                    <p>Sala reservada: ${salaReserva}</p>
-                    <p>Butacas reservadas: ${butacasReservadas}</p>
-                    <p>Email del cliente: ${userEmail}</p>
-                    <p>Precio de reserva: ${selectedPrice} €</p>
+                    <div>
+                        <p>Fecha de reserva: ${fechaReserva.toLocaleDateString()} a las ${fechaReserva.toLocaleTimeString()}</p>
+                        <p>Nombre de la pelicula: ${title}</p>
+                        <p>Sala reservada: ${salaReserva.selectedRoom}</p>
+                        <p>Butacas reservadas: ${butacasReservadas}</p>
+                        <p>Email del cliente: ${userEmail}</p>
+                        <p>Precio de reserva: ${selectedPrice} €</p>
+                    </div>
                   </body>
                 </html>
               `;
-          
-              // Abrir una nueva pestaña con el contenido del recibo
-              const newWindow = window.open("");
-              newWindow.document.write(reciboContent);
 
-        
+
             
+            // Abrir una nueva pestaña con el contenido del recibo
+            const newWindow = window.open("");
+            newWindow.document.write(reciboContent);
+
+            
+
+
+
             //Arreglar la suma de 1 en 1 cada vez que se envia un recibo a la base de datos, que el numeroRecibo sea 1, 2, 3, 4, etc...
             //Añadir tambien una ventana emergente que muestre el recibo por ejemplo en pdf, con el precio, email, sala y butacas reservadas
-            //Hacer tambien el precio por butaca y mostrarlo al cliente
+            //Añadir caja comentarios o de reseñas en el navbar seccion "Peliculas", donde salga el id del usuario  su valoración, etc. Con un input text-area
+            //donde el usuario pueda escribir una reseña sobre la pelicula
 
-            
             //set(ultimoReciboRef, { numeroRecibo: nuevoNumeroRecibo }, { merge: true });
-            
+
             // Remueve el listener
 
             unsubscribe();
-
         })
     }
-    
-    /*NO FUNCIONA HAY QUE ARREGLARLO
-    const cancelReserve = () => {
-
-        const reservaRef = doc(db, 'recibos');
-        updateDoc(reservaRef, {
-            numeroRecibo: deleteField(),
-            email: deleteField(),
-            salaReservada: deleteField()
-        })
-    }*/
 
     return (
         <Container>
+
             <h1 class='text-light'>Butacas disponibles</h1>
+
             <Row>
                 {seats.map((seat, index) => (
                     <Col key={index} xs={2} className="mb-3">
@@ -133,18 +125,14 @@ export default function CinemaRoom() {
                     </Col>
                 ))}
             </Row>
+
             <p class='text-light d-flex flex-row'>Butacas seleccionadas: {selectedSeats}</p>
             <p class='text-light d-flex align-content-end'>Precio: {selectedPrice}€</p>
+            
             <Button onClick={saveReceipt} className='m-2'>Conseguir Ticket</Button>
             
-            
-            {/*<Button onClick={"receipt"}>Informe Recibo</Button>*/}
-
-           {/* <Button onClick={ cancelReserve } className='m-2'>Cancelar reserva</Button> 
-           Tengo ya el boton de abajo BackButton para volver atras, por lo que no creo que haga falta este boton de cancelReserve
-           */} 
-
             <BackButton />
+
         </Container>
     );
 };
