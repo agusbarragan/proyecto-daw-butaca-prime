8ecd2bca58a75bc532511121512204c2697d971c
Sylrel <abl00021@hotmail.com>
2023-04-24 18:22:14 +0200

    Actualizando 24 abril por la tarde

--- src/functions/anularReserva.js
+++ src/functions/anularReserva.js
@@ -1,79 +1,76 @@
-import {  collection, deleteDoc, doc, getDoc, getDocs, getFirestore, query, where } from 'firebase/firestore'
-import React, { useState } from 'react'
-import firebaseApp, { auth } from '../firebase-config'
-import { Button, Container } from 'react-bootstrap';
-import { Modal } from 'react-bootstrap';
-import BackButton from '../components/BackButton';
-import NavigationBar from '../components/Navbar';
+import React, { useState, useEffect } from "react";
+import { getFirestore, onSnapshot, collection, query, where, deleteDoc, doc } from "firebase/firestore";
+import { auth } from "../firebase-config";
+import firebaseApp from "../firebase-config";
+import BackButton from "../components/BackButton";
+import NavigationBar from "../components/Navbar";
 
-export default function AnularReserva() {
+
+const AnularReservas = () => {
+    const [recibos, setRecibos] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     const db = getFirestore(firebaseApp);
-
-const docRef = doc(db, "recibos", "JCZVoU9fI4ElGY9bKjS6");
-const docSnap =  getDoc(docRef);
-
-if (docSnap) {
-  console.log("Document data:", docSnap);
-} else {
-  // doc.data() will be undefined in this case
-  console.log("No such document!");
-}
-
-    const [showModal, setShowModal] = useState(false);
-
-    /*const anularReserva = () => {
-    deleteDoc(doc(db, 'recibos', ));
-        
-    }*/
-
-  /*getDocs(q).then((querySnapshot) => {    Este metodo serviria para que aparecieran en pantalla 
-            const recibos = [];             las reservas de las personas logueadas pero no funciona
-            querySnapshot.forEach((doc) => {
-              recibos.push({ id: doc.id, data: doc.data() });
-            });
-            document.write(recibos);
-            //Aquí puedes mostrar los recibos en pantalla utilizando los datos en el array recibos
-          });*/
-
-    function anularReserva() {  //Esta funcion borra todos los recibos(encriptados) del usuario que este logueado en ese momento
-
-        const q = query(collection(db, 'recibos'),
-                where('email', '==', `${auth.currentUser.email}`)
-            );
-
-        getDocs(q).then((querySnapshot) => {
-            for (var i = 0; i < querySnapshot.docs.length; i++) {  
-                deleteDoc(querySnapshot.docs[i].ref);
-                console.log(querySnapshot.docs[i]);
-            }
-        })
+    
+    useEffect(() => {
+        const usuario = auth.currentUser.email;
+        const q = query(collection(db, "recibos"), where("email", "==", usuario));
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+          const data = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+          }));
+          setRecibos(data);
+          setLoading(false);
+        });
+        return unsubscribe;
+      }, [db]);
+      
+      
+    
+    const anularReserva = async (id) => {
+        try {
+            console.log(doc);
+          await deleteDoc(doc(db, 'recibos', id));
+          
+          console.log("Reserva anulada con éxito");
+        } catch (error) {
+          console.error("Error al anular la reserva", error);
+        }
+      };
+          
+    if (loading) {
+      return <div>Cargando...</div>;
     }
-
+    
     return (
-        
-    <Container>
-        
+        <>
         <NavigationBar />
-
-        <h1 className='text-light'>Reservas</h1>
-        <Button onClick={anularReserva} className='me-2'>Anular reserva</Button>
-        <Modal show={showModal} onHide={() => setShowModal(false)}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Información</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <p>Reserva realizada, disfrute de la pelicula.</p>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={() => setShowModal(false)}>
-                        Cerrar
-                        </Button>
-                    </Modal.Footer>
-                    </Modal>    
-
+      <div>
+        <h2 className="text-light">Mis reservas</h2>
+        <div className="row">
+          {recibos.map((recibo) => (
+            <div key={recibo.id} className="col-md-4 mb-4">
+                <div className="card">
+                    <div className="card-body">
+                    <h5 className="card-title">{recibo.email}</h5>
+                    <p className="card-text">Película: {recibo.nombrePelicula}</p>
+                    <p className="card-text">Butacas: {recibo.butacas}</p>
+                    <p className="card-text">Hora reserva: {recibo.salaReservada.hora}</p>
+                    <p className="card-text">Día: {recibo.fecha.toDate().toLocaleDateString()} a las {recibo.fecha.toDate().toLocaleTimeString()}</p>
+                    <p className="card-text">Total pagado: {recibo.precioReserva}</p>
+                </div>
+            <div className="card-footer">
+              <button onClick={() => anularReserva(recibo.id)} className="btn btn-danger">Anular reserva</button>
+            </div>
+            </div>
+            </div>
+          ))}
+        </div>
+      </div>
         <BackButton />
-
-    </Container>
-
-  )
-}
+      </>
+    );
+  };
+  
+  export default AnularReservas;
